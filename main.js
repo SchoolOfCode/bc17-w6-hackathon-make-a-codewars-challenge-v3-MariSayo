ae10372b4ee1b5fada6df5139dcae620f2459a24
Maria <maria.g1611@yahoo.com>
2024-08-09 12:24:05 +0100

    added return

--- main.js
+++ main.js
@@ -2,7 +2,11 @@
 
 export function rainbowColour() {
 
-}
+  return  {rainbowString,
+    originalLength,
+    finalLength,
+  }
+};
 
 
 //return this function as a string that excludes the colour indigo because that is not a colour of the rainbow
